690a01db531958e47e5d9be2c9bbcb484146765d
Sixuan Wu <sixuan@Sixuans-iMac.local>
2023-04-13 11:48:10 -0400

    update students_query.js so that it's not vulnerable to SQL injection attacks

--- students_query_parameter.js
+++ students_query_parameter.js
@@ -1,19 +1,37 @@
-const { Pool } = require('pg')
- 
-const pool = new Pool({
-  database: 'test_project'
-})
+const { Client } = require("pg");
 
-pool.query(`
-SELECT students.id as student_id, students.name as name
+const client = new Client({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+  port: 5432,
+});
+
+client.connect().then(() => {
+  console.log("Connected to database!");
+});
+
+const queryString = `
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-WHERE name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
-  })
-}).catch(err => console.error('query error', err.stack));
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+//Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
 
-pool.end()
+client
+  .query(queryString, values)
+  .then((res) => {
+    // console.log(res.rows);
+    res.rows.forEach((user) => {
+      console.log(
+        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+      );
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
